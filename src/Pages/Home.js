d1477091b9bf8263690f47b144470013deee089b
naseem173 <md.naseem0978@gmail.com>
2024-12-21 03:32:00 +0530

    Setting up sidebar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import Widgets from "./Widgets/Widgets";
+import Sidebar from "./Sidebar/sidebar";
+import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
-  return <div>Home page</div>;
+  const navigate = useNavigate();
+  const user = {
+    displayname: "bithead",
+    email: "bithead@gmail.com",
+  };
+  const handleout = async () => {
+    try {
+      navigate("/login");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div className="app">
+      <Sidebar handleout={handleout} user={user} />
+      <Outlet />
+      <Widgets />
+    </div>
+  );
 };
 
 export default Home;
